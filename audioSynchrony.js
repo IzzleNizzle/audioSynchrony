f3d80fd6cca2fe01bddcd734e7183119c87f6348
Iz P <pehisaac@gmail.com>
2020-01-11 15:57:54 -0700

    adding new files for react

---
+++ audioSynchrony.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from './pages/NavBar'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+
+
+export default function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route
+          path="/dashboard"
+          component={Dashboard}
+        />
+        <Route component={() => "404 No Match Found"} />
+      </Switch>
+    </Router>
+  )
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import useAudioSynchrony from './audioSynchrony';
+
+ReactDOM.render((
+  <button>Click to start</button>
+), document.getElementById('root'));
