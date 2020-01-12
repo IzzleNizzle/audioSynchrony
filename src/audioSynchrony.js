841dbd6dea6ac1844edf0e46019852659bba8011
Iz P <pehisaac@gmail.com>
2020-01-11 22:21:11 -0700

    updates

--- src/audioSynchrony.js
+++ src/audioSynchrony.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+// Step 1. Download Audio file
+import rockets40 from './sounds/rockets40.m4a'
+// import rockets20 from './sounds/rockets20.m4a'
+// const rockets40_ = new Audio(rockets40)
+// const rockets20_ = new Audio(rockets20)
 
 export default function audioSynchrony() {
 
-  // Step 1. Download Audio file
+  const [queueCount, setQueueCount] = useState(0)
+  const incQueue = () => setQueueCount(oldState => oldState++)
+  const decQueue = () => setQueueCount(oldState => oldState--)
+
+  // Create new sound file
+  const mkSound = () => new Audio(rockets40)
+
   return () => {
+    let s = mkSound()
+    s.play()
     console.log('====================================');
     console.log('hmm');
     console.log('====================================');
